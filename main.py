85de237c99fd6dab1ff79763047a6782d261502a
Anne Zonneveld <AnneZonneveld@air-van-anne.home>
2021-01-08 13:33:29 +0100

    flipped grid

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ if __name__ == "__main__":
     print(houses[1].type)
     print(test_grid.cells[0,0].x_coordinate)
     print(test_grid.cells[0,0].y_coordinate)
-    # test_grid.draw()
     test_grid.print_grid()
     
