2f3045eeadb8d7dc12426f42dcd4b502e1b2619c
Fleur Tervoort <fleurtervoort@MacBook-van-Fleur.local>
2021-01-08 16:03:44 +0100

    load water function completed

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     print(houses[1].type)
     print(test_grid.cells[0,0].x_coordinate)
     print(test_grid.cells[0,0].y_coordinate)
-    # test_grid.draw()
     test_grid.print_grid()
-    
+    water = test_grid.load_water("data/wijken/wijk_2.csv")
+    print(water)
