6c3c5ec87d90a06f23f1abdda0d34bcdde68ed89
Soeltjer <seike.appold@student.uva.nl>
2021-01-08 17:45:19 +0100

    Visualization: Adds water based on wijk, single steps on axis

--- code/visualization/visualize.py
+++ code/visualization/visualize.py
@@ -1,24 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def visualize(width, depth, house):
+def visualize(width, depth, grid, wijk):
 
-    plt.axis([0, width, 0, depth])  
-
+    plt.axis([0, grid.width, 0, grid.depth]) 
+    plt.xticks(np.arange(0, grid.width + 1, 1))
+    plt.yticks(np.arange(0, grid.depth + 1, 1)) 
     # how start from bottom left corner from https://stackoverflow.com/questions/44395838/how-to-make-0-0-on-matplotlib-graph-on-the-bottom-left-corner
-    plt.xlim([0, width])
-    plt.ylim([0, depth])
-
+    plt.xlim([0, grid.width])
+    plt.ylim([0, grid.depth])
     plt.grid(True)
-
+    
     # how to draw rectangle in diagram from https://www.codespeedy.com/how-to-draw-shapes-in-matplotlib-with-python/
-    x = 4
-    y = 0
+    
+    """
+    Todo: load house(s)
+    test_house = house.House("single", 1)
+    h1 = plt.Rectangle((x, y), 8, 8, fc="orange")
+    """
 
-    h1 = plt.Rectangle((x, y), house.width, house.depth, fc="orange")
-    w1 = plt.Rectangle((0, 0), 3.2, 18, fc="blue")
+    # load correct map
+    if wijk == "wijk_1":
+        water = grid.load_water("data/wijken/wijk_1.csv")
+    elif wijk == "wijk_2":
+        water = grid.load_water("data/wijken/wijk_2.csv")
+    else:
+        water = grid.load_water("data/wijken/wijk_3.csv")
+    
+    # how to loop through nested dict from https://www.learnbyexample.org/python-nested-dictionary/#:~:text=Access%20Nested%20Dictionary%20Items,key%20in%20multiple%20square%20brackets.&text=If%20you%20refer%20to%20a,dictionary%2C%20an%20exception%20is%20raised.&text=To%20avoid%20such%20exception%2C%20you,special%20dictionary%20get()%20method.
+    for ident, coordinates in water.items():
+        # *0.1 adjusts coordinates to smaller map for testing
+        bottom_x = int(water[ident].get('bottom_left_x'))*0.1
+        bottom_y = int(water[ident].get('bottom_left_y'))*0.1
+        top_x = int(water[ident].get('top_right_x'))*0.1
+        top_y = int(water[ident].get('top_right_y'))*0.1
+        print(f"{bottom_x}, {bottom_y}, {top_x}, {top_y}")
 
-    plt.gca().add_patch(h1)
-    plt.gca().add_patch(w1)
+        water_vis = plt.Rectangle((bottom_x, bottom_y), top_x, top_y, fc="blue")
+        # plt.gca().add_patch(h1)
+        plt.gca().add_patch(water_vis)
 
     plt.savefig('visualization.png')
--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from code.visualization import visualize as vis
 
 if __name__ == "__main__":
     test_grid = grid.Grid(20)
+    """
     print(test_grid.cells[0,3].x_coordinate)
     print(test_grid.cells[0,3].y_coordinate)
+    """
 
-    test_house = house.House("single", 1)
+    # visualize wijk_1
+    vis.visualize(test_grid.width, test_grid.depth, test_grid, "wijk_3")
 
-    # visualization
-    vis.visualize(test_grid.width, test_grid.depth, test_house)
-
+    """
     test_grid = grid.Grid(20)
     houses = test_grid.all_houses
     print(f"houses: {houses}")
@@ -20,3 +21,4 @@ if __name__ == "__main__":
     test_grid.print_grid()
     water = test_grid.load_water("data/wijken/wijk_2.csv")
     print(water)
+    """
