d3f6039914c21a84d096e6ba593f4b91dff211b7
Soeltjer <seike.appold@student.uva.nl>
2021-01-08 13:27:01 +0100

    Added visualize function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from code.classes import grid
+from code.visualization import visualize as vis
 
 if __name__ == "__main__":
     test_grid = grid.Grid()
     print(test_grid.cells[0,3].x_coordinate)
     print(test_grid.cells[0,3].y_coordinate)
-    
+
+    # visualization
+    vis.visualize()
