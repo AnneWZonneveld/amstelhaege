d03f452a90ec99a5052f3405c431d518ff864157
Soeltjer <seike.appold@student.uva.nl>
2021-01-08 14:27:58 +0100

    Can add rectangles

--- code/visualization/visualize.py
+++ code/visualization/visualize.py
@@ -1,14 +1,19 @@
 import matplotlib.pyplot as plt
 
 def visualize(width, depth):
-    
-    x_max = width
-    y_max = depth
-    w = range(x_max)
-    d = range(y_max)
+
+    w = range(width)
+    d = range(depth)
 
     plt.plot([w], [d])  
-    plt.xlim([0, x_max])
-    plt.ylim([0, y_max])
+    
+    # how start from bottom left corner from https://stackoverflow.com/questions/44395838/how-to-make-0-0-on-matplotlib-graph-on-the-bottom-left-corner
+    plt.xlim([0, width])
+    plt.ylim([0, depth])
+    
     plt.grid(True)
+
+    # how to draw rectangle in diagram from https://www.codespeedy.com/how-to-draw-shapes-in-matplotlib-with-python/
+    house = plt.Rectangle((0, 0), 2, 2.32, fc="blue")
+    plt.gca().add_patch(house)
     plt.savefig('visualization.png')
