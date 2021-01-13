3f3ceaf062a53ea72ff36f497d1ad7ca71e077c3
Soeltjer <seike.appold@student.uva.nl>
2021-01-13 14:04:54 +0100

    Tests for adding visualisation of mandatory free space

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ from code.algorithms import randomize
 if __name__ == "__main__":
 
 	# Create grid
-    test_grid = grid.Grid(20, "data/wijken/wijk_1.csv")
-    test_grid.print_grid()
+    # test_grid = grid.Grid(20, "data/wijken/wijk_1.csv")
+    # test_grid.print_grid()
 
-    test_grid.print_grid()
+    # test_grid.print_grid()
     # water = test_grid.load_water("data/wijken/wijk_2.csv")
     # print(water)
     
     # Randomize algorithm
 
-    random_config = randomize.random_assignment(test_grid)
+    # random_config = randomize.random_assignment(test_grid)
 
-    print("New grid:")
-    random_config.print_grid()
+    # print("New grid:")
+    # random_config.print_grid()
 
     # Visualize case
-    vis.visualize(random_config)
+    # vis.visualize(test_grid)
 
     # # Create csv output file
     # test_grid.create_output()
