4289994cc0a0b8412a7b48b48e23e826092aa1b0
Anne Zonneveld <AnneZonneveld@air-van-anne.home>
2021-01-14 10:44:57 +0100

    started calculating free space

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from code.algorithms import randomize
 
 if __name__ == "__main__":
 
-	Create grid
+	#Create grid
     test_grid = grid.Grid(20, "data/wijken/wijk_1.csv")
     test_grid.print_grid()
 
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     water = test_grid.load_water("data/wijken/wijk_2.csv")
     print(water)
     
-    Randomize algorithm
+    #Randomize algorithm
 
     random_config = randomize.random_assignment(test_grid)
 
     print("New grid:")
     random_config.print_grid()
 
-    Visualize case
+    #Visualize case
     vis.visualize(test_grid)
 
     # Create csv output file
