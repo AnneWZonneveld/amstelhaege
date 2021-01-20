1eb7ac572a79ee9a85e34c1cf247ccecef45fa6f
Soeltjer <seike.appold@student.uva.nl>
2021-01-20 15:36:10 +0100

    Minor changes in visualize

--- main.py
+++ main.py
@@ -1,44 +1,24 @@
 from code.classes import grid, house # niet nodig? 
 from code.visualization import visualize as vis
 from code.algorithms import randomize as rz
-from code.algorithms import greedy as gr
-
+# from code.algorithms import greedy as gr
 
 if __name__ == "__main__":
 
-	quantity = 20
-	map_source = "data/wijken/wijk_3.csv"
+    quantity = 20
+    map_source = "data/wijken/wijk_3.csv"
 
-	# Create grid from data
-	test_grid = grid.Grid(quantity, map_source)
+    # Create grid from data
+    test_grid = grid.Grid(quantity, map_source)
 
-	# ----------------------- Randomize algorithm ---------------------------------
-	# random_config = rz.random_assignment(test_grid)
+    # Value of grid 
+    # print(f"Value greedy config: {greedy.graph.value}")
+    random = rz.random_assignment(test_grid)
+    # Visualize case
+    vis.visualize(random)
 
-	# # print("New grid:")
-	# # print(random_config.cells)
-
-	# # Value of grid
-	# print(f"Value random config: {random_config.value}")
-
-	# # Visualize case
-	# vis.visualize(random_config)
-
-	# # Create csv output file
-	# random_config.create_output()
-
-	# ------------------------ Greedy algorithm -----------------------------------
-	greedy = gr.Greedy(test_grid)
-	greedy.run()
-
-	# Value of grid 
-	print(f"Value greedy config: {greedy.graph.value}")
-
-	# Visualize case
-	vis.visualize(greedy.graph)
-
-	# Create csv output file
-	greedy.graph.create_output()
+    # Create csv output file
+    # greedy.graph.create_output()
 
 
 
