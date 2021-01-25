29154ccd6f4f2218c03b14be1dcc2534d01ff662
Fleur Tervoort <fleurtervoort@MacBook-van-Fleur.local>
2021-01-25 13:20:08 +0100

    clean up grid.py and requirements.txt

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -1,18 +1,20 @@
 import csv
-import itertools
-import numpy as np
 from .house import House
 from .water import Water
 from shapely.geometry import Point
-from code.classes.mandatory import MandatoryFreeSpace
-from shapely.geometry import Point
-import code.algorithms.randomize as rz
-# from IPython import embed;
+
+# Constants
+MAX_EXTRA_FREE = 250
+GRID_WIDTH = 180
+GRID_DEPTH = 160
+PERC_SINGLE = 0.6
+PERC_BUNGALOW = 0.25
+PERC_MAISON = 0.15
 
 class Grid():
     def __init__(self, quantity, source_file):
-        self.width = 180
-        self.depth = 160
+        self.width = GRID_WIDTH
+        self.depth = GRID_DEPTH
         self.quantity = quantity
         self.water = source_file
         self.all_houses = self.load_houses()
@@ -45,9 +47,9 @@ class Grid():
         """
         
         # Determine quantity for each house type based on total quantity
-        q_single = int(0.6 * self.quantity)
-        q_bungalow = int(0.25 * self.quantity)
-        q_maison = int(0.15 * self.quantity)
+        q_single = int(PERC_SINGLE * self.quantity)
+        q_bungalow = int(PERC_BUNGALOW * self.quantity)
+        q_maison = int(PERC_MAISON * self.quantity)
 
         all_houses = []
         id_counter = 1
@@ -79,7 +81,7 @@ class Grid():
         all_water = []
 
         # Load coordinates of water surface(s) from source file
-        with open(self.water, 'r') as in_file:
+        with open(self.water, "r") as in_file:
             # Skip header
             next(in_file)
 
@@ -100,10 +102,10 @@ class Grid():
                 water.id = id_counter
 
                 # Save water coordinates in dict
-                water.coordinates = {'bottom_left': (int(strip_items[1]), int(strip_items[4])),
-                                    'bottom_right': (int(strip_items[3]), int(strip_items[4])),
-                                    'top_left': (int(strip_items[1]), int(strip_items[2])),
-                                    'top_right': (int(strip_items[3]), int(strip_items[2]))}
+                water.coordinates = {"bottom_left": (int(strip_items[1]), int(strip_items[4])),
+                                    "bottom_right": (int(strip_items[3]), int(strip_items[4])),
+                                    "top_left": (int(strip_items[1]), int(strip_items[2])),
+                                    "top_right": (int(strip_items[3]), int(strip_items[2]))}
         
                 # Add water object to list
                 all_water.append(water)   
@@ -114,13 +116,13 @@ class Grid():
 
     def define_object_coordinates(self, coordinates):
         """
-        Returns a list of coordinates for a specific object.
+        Returns a list of all coordinates for a specific object.
         """
 
         object_coordinates = []
 
-        for column in range(coordinates['top_left'][1], coordinates['bottom_right'][1]):
-            for row in range(coordinates['top_left'][0], coordinates['bottom_right'][0]):
+        for row in range(coordinates["top_left"][0], coordinates["bottom_right"][0]):
+            for column in range(coordinates["top_left"][1], coordinates["bottom_right"][1]):
 
                 current_coordinate = (row, column)
                 object_coordinates.append(current_coordinate)
@@ -163,8 +165,7 @@ class Grid():
 
     def assignment_house(self, house):
         """ 
-        Assigns house to grid, based on coordinates of cell. Returns the new 
-        grid.
+        Assigns house to grid, based on its coordinates.
         """
 
         # Add coordinates to grid 
@@ -186,76 +187,61 @@ class Grid():
 
         print(f"Calculating extra free meters for: {house}")
 
-        # MISSCHIEN NOG AANPASSEN?
-        house.extra_free = 10000
+        # Set extra free meters to a bit more than the possible maximum
+        house.extra_free = MAX_EXTRA_FREE
 
-        # For all houses other than the selected one
+        # For all placed houses other than the selected one
         for other_house in self.all_houses:
             if other_house != house and other_house.placed:
                 
-                # other_house linksboven van house
-                if other_house.outer_house_coordinates['bottom_right'][0] <= house.outer_house_coordinates['top_left'][0] and other_house.outer_house_coordinates['bottom_right'][1] <= house.outer_house_coordinates['top_left'][1]:
-                    house_x = house.outer_house_coordinates['top_left'][0]
-                    house_y = house.outer_house_coordinates['top_left'][1]
-                    other_house_x = other_house.outer_house_coordinates['bottom_right'][0]
-                    other_house_y = other_house.outer_house_coordinates['bottom_right'][1]
-                    distance = Point(house_x,house_y).distance(Point(other_house_x,other_house_y))
-                    extra_free_space = distance - house.min_free
+                # If other_house is at the top left of the house
+                if other_house.outer_house_coordinates["bottom_right"][0] <= house.outer_house_coordinates["top_left"][0] and other_house.outer_house_coordinates["bottom_right"][1] <= house.outer_house_coordinates["top_left"][1]:
+                    house_xy = house.outer_house_coordinates["top_left"]
+                    other_house_xy = other_house.outer_house_coordinates["bottom_right"]
+                    distance = Point(house_xy[0],house_xy[1]).distance(Point(other_house_xy[0],other_house_xy[1]))
 
-                # other_house rechtsboven van house
-                elif other_house.outer_house_coordinates['bottom_left'][0] >= house.outer_house_coordinates['top_right'][0] and other_house.outer_house_coordinates['bottom_left'][1] <= house.outer_house_coordinates['top_right'][1]:
-                    house_x = house.outer_house_coordinates['top_right'][0]
-                    house_y = house.outer_house_coordinates['top_right'][1]
-                    other_house_x = other_house.outer_house_coordinates['bottom_left'][0]
-                    other_house_y = other_house.outer_house_coordinates['bottom_left'][1]
-                    distance = Point(house_x,house_y).distance(Point(other_house_x,other_house_y))
-                    extra_free_space = distance - house.min_free
+                # If other_house is at the top right of the house
+                elif other_house.outer_house_coordinates["bottom_left"][0] >= house.outer_house_coordinates["top_right"][0] and other_house.outer_house_coordinates["bottom_left"][1] <= house.outer_house_coordinates["top_right"][1]:
+                    house_xy = house.outer_house_coordinates["top_right"]
+                    other_house_xy = other_house.outer_house_coordinates["bottom_left"]
+                    distance = Point(house_xy[0],house_xy[1]).distance(Point(other_house_xy[0],other_house_xy[1]))
                 
-                # other_house rechtsonder van house
-                elif other_house.outer_house_coordinates['top_left'][0] >= house.outer_house_coordinates['bottom_right'][0] and other_house.outer_house_coordinates['top_left'][1] >= house.outer_house_coordinates['bottom_right'][1]:
-                    house_x = house.outer_house_coordinates['bottom_right'][0]
-                    house_y = house.outer_house_coordinates['bottom_right'][1]
-                    other_house_x = other_house.outer_house_coordinates['top_left'][0]
-                    other_house_y = other_house.outer_house_coordinates['top_left'][1]
-                    distance = Point(house_x,house_y).distance(Point(other_house_x,other_house_y))
-                    extra_free_space = distance - house.min_free
+                # If other_house is at the bottom right of the house
+                elif other_house.outer_house_coordinates["top_left"][0] >= house.outer_house_coordinates["bottom_right"][0] and other_house.outer_house_coordinates["top_left"][1] >= house.outer_house_coordinates["bottom_right"][1]:
+                    house_xy = house.outer_house_coordinates["bottom_right"]
+                    other_house_xy = other_house.outer_house_coordinates["top_left"]
+                    distance = Point(house_xy[0],house_xy[1]).distance(Point(other_house_xy[0],other_house_xy[1]))
 
-                # other_house linksonder van house
-                elif other_house.outer_house_coordinates['top_right'][0] <= house.outer_house_coordinates['bottom_left'][0] and other_house.outer_house_coordinates['top_right'][1] >= house.outer_house_coordinates['bottom_left'][1]:
-                    house_x = house.outer_house_coordinates['bottom_left'][0]
-                    house_y = house.outer_house_coordinates['bottom_left'][1]
-                    other_house_x = other_house.outer_house_coordinates['top_right'][0]
-                    other_house_y = other_house.outer_house_coordinates['top_right'][1]
-                    distance = Point(house_x,house_y).distance(Point(other_house_x,other_house_y))
-                    extra_free_space = distance - house.min_free
+                # If other_house is at the bottom left of the house
+                elif other_house.outer_house_coordinates["top_right"][0] <= house.outer_house_coordinates["bottom_left"][0] and other_house.outer_house_coordinates["top_right"][1] >= house.outer_house_coordinates["bottom_left"][1]:
+                    house_xy = house.outer_house_coordinates["bottom_left"]
+                    other_house_xy = other_house.outer_house_coordinates["top_right"]
+                    distance = Point(house_xy[0],house_xy[1]).distance(Point(other_house_xy[0],other_house_xy[1]))
 
-                # other_house boven house
-                elif other_house.outer_house_coordinates['bottom_left'][1] < house.outer_house_coordinates['top_left'][1]:
-                    distance = house.outer_house_coordinates['top_left'][1] - other_house.outer_house_coordinates['bottom_left'][1]
-                    extra_free_space = distance - house.min_free
+                # If other_house is above the house
+                elif other_house.outer_house_coordinates["bottom_left"][1] < house.outer_house_coordinates["top_left"][1]:
+                    distance = house.outer_house_coordinates["top_left"][1] - other_house.outer_house_coordinates["bottom_left"][1]
 
-                # other_house rechts van house
-                elif other_house.outer_house_coordinates['bottom_left'][0] > house.outer_house_coordinates['bottom_right'][0]:
-                    distance = other_house.outer_house_coordinates['bottom_left'][0] - house.outer_house_coordinates['bottom_right'][0]
-                    extra_free_space = distance - house.min_free
+                # If other_house is to the right of the house
+                elif other_house.outer_house_coordinates["bottom_left"][0] > house.outer_house_coordinates["bottom_right"][0]:
+                    distance = other_house.outer_house_coordinates["bottom_left"][0] - house.outer_house_coordinates["bottom_right"][0]
 
-                # other_house onder house
-                elif other_house.outer_house_coordinates['top_left'][1] > house.outer_house_coordinates['bottom_left'][1]:
-                    distance = other_house.outer_house_coordinates['top_left'][1] - house.outer_house_coordinates['bottom_left'][1]
-                    extra_free_space = distance - house.min_free
+                # If other_house is below the house
+                elif other_house.outer_house_coordinates["top_left"][1] > house.outer_house_coordinates["bottom_left"][1]:
+                    distance = other_house.outer_house_coordinates["top_left"][1] - house.outer_house_coordinates["bottom_left"][1]
 
-                # other_house links van house
-                elif other_house.outer_house_coordinates['bottom_right'][0] < house.outer_house_coordinates['bottom_left'][0]:
-                    distance = house.outer_house_coordinates['bottom_left'][0] - other_house.outer_house_coordinates['bottom_right'][0]
-                    extra_free_space = distance - house.min_free
+                # If other_house is to the left of the house
+                elif other_house.outer_house_coordinates["bottom_right"][0] < house.outer_house_coordinates["bottom_left"][0]:
+                    distance = house.outer_house_coordinates["bottom_left"][0] - other_house.outer_house_coordinates["bottom_right"][0]
+                
+                # Calculate extra free space
+                extra_free_space = distance - house.min_free
 
-                print(f"Extra free space: {extra_free_space}")
-
-                # If extra free space is shorter than the current one, replace
+                # If extra free space is less than the current amount, replace
                 if extra_free_space < house.extra_free:
                     house.extra_free = extra_free_space
                     
-                    print(f"NEW SHORTEST EXTRA FREE SPACE: {house.extra_free}")
+        print(f"Extra free space for {house}: {house.extra_free}")
 
     def calculate_worth(self):
         """
@@ -282,17 +268,21 @@ class Grid():
                 # Add worth of house to total net worth of the map
                 total_networth += worth_house
 
-        # Assign value to grid (MISSCHIEN NIET HANDIG VOOR GREEDY?)
-        self.value = total_networth
+        # Round to nearest integer
+        total_networth = int(round(total_networth))
 
+        # Assign value to grid
+        self.value = total_networth
+        
         return total_networth
 
     def create_output(self, name):
         """
-        Creates csv-file with results from running an algorithm to place houses.
+        Creates a csv-file with results from running an algorithm to place 
+        houses.
         """
 
-        with open(f'data/output/csv/output_{name}.csv', 'w', newline='') as file:
+        with open(f"data/output/csv/output_{name}.csv", "w", newline='') as file:
             writer = csv.writer(file)
 
             # Create header
@@ -301,18 +291,18 @@ class Grid():
 
             # Add location of water to csv file
             for water_object in self.load_water():
-                bottom_left = water_object.coordinates['bottom_left']
-                bottom_right = water_object.coordinates['bottom_right']
-                top_left = water_object.coordinates['top_left']
-                top_right = water_object.coordinates['top_left']
+                bottom_left = water_object.coordinates["bottom_left"]
+                bottom_right = water_object.coordinates["bottom_right"]
+                top_left = water_object.coordinates["top_left"]
+                top_right = water_object.coordinates["top_left"]
                 water_list = [water_object, bottom_left, bottom_right, top_left, top_right, "WATER"]
                 writer.writerow(water_list)
             
             for house in self.all_houses:
-                bottom_left = house.outer_house_coordinates['bottom_left']
-                bottom_right = house.outer_house_coordinates['bottom_right']
-                top_left = house.outer_house_coordinates['top_left']
-                top_right = house.outer_house_coordinates['top_left']
+                bottom_left = house.outer_house_coordinates["bottom_left"]
+                bottom_right = house.outer_house_coordinates["bottom_right"]
+                top_left = house.outer_house_coordinates["top_left"]
+                top_right = house.outer_house_coordinates["top_left"]
                 house_list = [f"{house.type}_{house.id}", bottom_left, bottom_right, top_left, top_right, house.type.upper()]
                 writer.writerow(house_list)
 
