fb38007f9c7b6d882e64bef0571417a2eacc470a
Soeltjer <seike.appold@student.uva.nl>
2021-01-27 19:14:07 +0100

    Fixed comments in greedy

--- code/visualization/visualize.py
+++ code/visualization/visualize.py
@@ -1,105 +1,119 @@
+################################################################################
+# visualize.py
+#
+# Programmeertheorie
+# Anne Zonneveld, Fleur Tervoort, Seike Appold
+#
+# Generates visualizations of a solution for the Amstelhaege case.
+################################################################################
+
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 from matplotlib.collections import PatchCollection
-from IPython import embed
 
 def hist_plot(values, map_name, quantity, name):
     """
-    Creates a histogram.
+    Creates a histogram that represents the total value of a map in relation to
+    the number of times an algorithm has been run. (?)
     """
     
     fig, ax = plt.subplots()
-
     q_iterations = len(values)
 
-    plt.hist(values)
     plt.title(f"{name.capitalize()}: {q_iterations} iterations")
     plt.xlabel("Value (€)")
     plt.ylabel("Frequency")
 
+    # Draw diagram based on passed on results
+    plt.hist(values)
+
     path = os.path.join(".","data", "output", f"{map_name}", f"{quantity}", "figures", "analysis")
 
     if not os.path.exists(path):
         os.makedirs(path)
 
+    # Save diagram to matching directory
     new_path = os.path.join(path, f"{name}_hist.png" )
     plt.savefig(new_path)
 
+
 def iteration_plot(values, map_name, quantity, hc_type, start_state):
     """
-    Creates a plot that shows the development of grid value over iterations.
+    Creates a plot that shows how the value of a map is distributed across
+    the development of grid value over iterations. (?)
     """
 
     fig, ax = plt.subplots()
-
-    # Determine x
-    q_iterations = len(values)
-    x = np.arange(1, q_iterations + 1)
-
-    plt.plot(x, values)
     plt.title(f"Hillclimber; type {hc_type}")
-    plt.xticks(np.arange(0, q_iterations +1, int(0.25*q_iterations)))
     plt.xlabel("Iterations")
     plt.ylabel("Value (€)")
+
+    q_iterations = len(values)
+    iterations = np.arange(1, q_iterations + 1)
+
+     # Draw plot based on passed on results and iterations
+    plt.plot(iterations, values)
+    plt.xticks(np.arange(0, q_iterations +1, int(0.25*q_iterations)))
 
     path = os.path.join(".","data", "output", f"{map_name}", f"{quantity}", "figures", "analysis")
 
     if not os.path.exists(path):
         os.makedirs(path)
-        
+    
+    # Save diagram to matching directory
     new_path = os.path.join(path, f"{start_state}_hillclimber_{hc_type}.png")
     plt.savefig(new_path)
 
+
 def visualize(grid, map_name, quantity, name=""):
-    """
-    Creates a diagram that displays a map of Amstelhaege based on
-    a given grid object.
-    """
+        """
+        Creates a diagram that displays a map of Amstelhaege based on
+        a given grid object.
+        """
 
-    # Create diagram of size of map
-    plt.plot([0, grid.width, grid.depth, 0])
-    plt.xlabel("Width")
-    plt.ylabel("Depth")
+        # Create diagram of size of map
+        plt.plot([0, grid.width, grid.depth, 0])
+        plt.title(f"{name.capitalize()}: €{str(round(grid.value, 2))}")
+        plt.xlabel("Width")
+        plt.ylabel("Depth")
 
-    # Prepare for drawing multiple objects at once
-    fig, ax = plt.subplots()
-    objects = []
+        # Prepare for drawing multiple objects at once
+        fig, ax = plt.subplots()
+        objects = []
 
-    # Set scaling properties
-    plt.xticks(np.arange(0, grid.width + 1, 10))
-    plt.yticks(np.arange(0, grid.depth + 1, 10)) 
-    plt.xlim([0, grid.width])
-    plt.ylim([grid.depth, 0])
+        # Set scaling properties
+        plt.xticks(np.arange(0, grid.width + 1, 10))
+        plt.yticks(np.arange(0, grid.depth + 1, 10)) 
+        plt.xlim([0, grid.width])
+        plt.ylim([grid.depth, 0])
 
-    # Set grid properties
-    plt.grid(True)
+        # Make grid visible on map
+        plt.grid(True)
 
-    # Title
-    plt.title(f"{name.capitalize()}: €{str(round(grid.value, 2))}")
+        # Create representation of water
+        water_coord= grid.load_water()
+        water = draw_water(water_coord)
 
-    # Create representation of water
-    water_coord= grid.load_water()
-    water = draw_water(water_coord)
+        # Create representation of houses
+        houses_coord = grid.all_houses
+        houses = draw_houses(houses_coord, grid)
 
-    # Create representation of houses
-    houses_coord = grid.all_houses
-    houses = draw_houses(houses_coord, grid)
+        # Add water and houses to diagram
+        objects.extend(water + houses)
+        representations = PatchCollection(objects, match_original=True)
+        ax.add_collection(representations)
 
-    # Add water and houses to diagram
-    objects.extend(water + houses)
-    representations = PatchCollection(objects, match_original=True)
-    ax.add_collection(representations)
+        path = os.path.join(".","data", "output", f"{map_name}", f"{quantity}", "figures", "maps")
 
-    # Save diagram to current directory
-    path = os.path.join(".","data", "output", f"{map_name}", f"{quantity}", "figures", "maps")
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-    if not os.path.exists(path):
-        os.makedirs(path)
+        # Save map to matching directory
+        new_path = os.path.join(path, f"{name}.png")
+        plt.savefig(new_path)
 
-    new_path = os.path.join(path, f"{name}.png")
-    plt.savefig(new_path)
-    
+
 def draw_water(water_coord):
     """
     Returns a list of patches based on given coordinates that represent the
@@ -116,6 +130,7 @@ def draw_water(water_coord):
         water.append(plt.Rectangle(bottom_left, width, height, fc="b"))
     
     return water
+
 
 def draw_houses(houses_coord, grid):
     """
@@ -153,3 +168,6 @@ def draw_houses(houses_coord, grid):
             houses.extend((house, free_space))
     
     return houses
+
+
+    
